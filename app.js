fe7a28e2c5e6fa8f39debeebe0b66e0a3dcd4183
Pamela Anderson <pamelaanderson519@yahoo.com>
2018-10-05 14:11:17 -0400

    added src files to html and cleaned up somethings

--- app.js
+++ app.js
@@ -1,60 +1,107 @@
-let terms = "";
-let urlBase = "http://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=";
-let authKey = "d413b797e2ef42889944fd71f193f81b" ;
-let beginDate = 0;
-let endDate = 0;
+$(function () {
 
-$("#sumbit").click(function() {
-    $("#articleArea").empty();
-    terms = $("#searchTerms").val().trim();
-    terms = terms.replace();
-    numberOfArticles = $('#recordNum option:selected').val()
-})
-// var apiKey = '39ed6293008242c39c5931e159dde131';
-// var keyParam = '&api_key=';
-// var qParam = '?q=';
-// var beginParam = '?begin_date=';
-// var endParam = '?end_date=';
-// var pageParam = '?page=';
-// var baseUrl = 'https://api.nytimes.com/svc/search/v2/articlesearch.json' ;
-// var testQ = "";
-
-// $("#sender").on("click", function (){
-//     var userSearch = $("#exampleInputEmail1").val().trim();
-//     //var userRecords = $("exampleSelect1").val().trim();
-//     var queryURL = baseUrl + qParam + userSearch + keyParam + apiKey;
-
-    // $.ajax({
-    //     url: queryURL,
-    //     method: "GET"
-    // }).then(function(response){
-    //     console.log(response);
-    //     var source = response.response.docs;
-    //     for(var i = 0; i < source.length; i++){
-
-const searchRecords = function () {}
+    const displayInfo = function () {
+        $.ajax({
+            url: 'https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=6ccb734888db4a05bdc082173279ab87',
+            method: 'GET'
+        }).then(function (response) {
+            console.log(response)
 
 
-    const val1 = $('#val1').val();
-    const val2 = $('#val2').val();
-    const val3 = $('#val3').val ();
+            const resultDiv = $('<div>').addClass('result');
 
-    
+            const articleHeadline = response.response.docs[0].headline.main;
+
+            const headlineHolder = $('<p>').text(`<h4>${articleHeadline}</h4>`);
+            resultDiv.append(headlineHolder);
+
+            const author = response.docs[0].headline.name;
+
+            const authorHolder = $('<p>').text(`<h4>${author}</h4>`);
+            resultDiv.append(authorHolder);
+
+            const date = response.docs[0].multimedia.pub_date;
+            const dateHolder = $('<p>').text(`<h4>${date}</h4>`);
+
+            console.log(date);
+
+        })
+    }
 
 
 
-    $("#search").click(function() {
-        $("#articleArea").empty();
-        terms = $("#searchTerms").val().trim();
-        terms = terms.replace(/ /g, "+");
-        numberOfArticles = $("#selectOptions option:selected").val()
-    
+
+    const selectVal = function () {
+        const val1 = $('#selectOptions').text();
+        console.log(val1);
+    }
+
+    $('#search').on('click', selectVal)
+
+    render();
+
+
+    const selectEmpty = function () {
+        $('#selectOptions').empty();
+        console.log(selectEmpty);
+    }
+
+
+    $("#articleArea").append(div)
+
+    // $("#clear").click(function () {
+    //     $("#articleArea").empty();
+
+    //     terms = $("#searchTerms").val().trim();
+    //     terms = terms.replace(/ /g, "+");
+
+
+
+    //     $("#search").click(function () {
+    //         $("#articleArea").empty();
+    //     })
+
+    //     displayInfo();
+
+
+
+
+
+        // ======================= NOTES =========================
+        // $("#sumbit").click(function() {
+        //     $("#articleArea").empty();
+        //     terms = $("#searchTerms").val().trim();
+        //     terms = terms.replace();
+        //     numberOfArticles = $('#recordNum option:selected').val()
+        // })
+
+
+        // $("#sender").on("click", function (){
+        //     var userSearch = $("#exampleInputEmail1").val().trim();
+        //     //var userRecords = $("exampleSelect1").val().trim();
+        //     var queryURL = baseUrl + qParam + userSearch + keyParam + apiKey;
+
+
+
+
+
+
+
+
+
+
+        //     $("#search").click(function() {
+        //         $("#articleArea").empty();
+        //         terms = $("#searchTerms").val().trim();
+        //         terms = terms.replace(/ /g, "+");
+        //         numberOfArticles = $("#selectOptions option:selected").val()
+
         // console.log( $("#searchTerms").val() )
         // console.log( $("#recordNum option:selected").val() );
-    
+
         // Need to make date..
-        beginDate = $("#startYear").val()
-        endDate = $("#endYear").val()
+        // beginDate = $("#startYear").val()
+        // endDate = $("#endYear").val()
 
 
-        
+    });
