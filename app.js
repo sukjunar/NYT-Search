62a0b671b8a95c2de8f8af12f91019800b825de5
Pamela Anderson <pamelaanderson519@yahoo.com>
2018-10-23 08:51:52 -0400

    Added css to the page

--- app.js
+++ app.js
@@ -1,101 +1,106 @@
-$(document).ready(function () {
+const render = function (NYTData) {
 
-    function clearFields() {
-        $('#searchInput').val("");
-        $('#startYearInput').val("");
-        $('#endYearInput').val("");
-        console.log($('#searchInput').val());
+    const numArticles = $('#article-count').val();
 
+    const articles = NYTData.response.docs.slice(0, numArticles);
+
+      for (let i = 0; i < articles.length; i++){
+        $('#article-section').append(buildArticle(articles[i]));
+      }
+
+  }
+
+  const buildArticle = function(article) {
+
+    const articleLink = $('<a>')
+    .attr('href', article.web_url)
+    .attr('target', '_blank');
+    
+
+    // If the article has a headline, append to articleLink
+    const headline = article.headline;
+
+    if (headline && headline.main) {
+      articleLink.append($('<h3>').text(headline.main));
     }
 
-    const search = function() {
-        const apikey = "d413b797e2ef42889944fd71f193f81b";
-        const searchInput = $('#searchInput').val();
-        const numberTerm = $('option:selected').val();
-        const startYearInput = $('#startYearInput').val() + `0101`;
-        const endYearInput = $('#endYearInput').val() + `1231`;
+    const byline = article.byline;
 
-        var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    if (byline && byline.original) {
+      articleLink.append($('<h5>').text(byline.original));
+    }
 
-        url += '?' + $.param({
-            'api-key': apikey,
-            'q': searchinput,
-            'begin_date': startYearInput,
-            'end_date': endYearInput,
-            'page' : 0,
-        });
+    const section = article.section_name;
 
-        $.ajax({
-            url: url,
-            method: 'GET'
-        }).done(function (response) {
+    if (section) {
+      articleLink.append($('<h5>').text(`Section: ${section}`));
+    }
+
+    const pubDate = article.pub_date;
+
+    if (pubDate) {
+      articleLink.append($('<p>').text(formatDate(article.pub_date)));
+    }
+
+
+    const articleListItem = $('<li>')
+    articleListItem.append(articleLink)
+
+    return articleListItem;
+  }
+
+  const formatDate = function(date) {
+
+    const dateObj = new Date(date);
+    const month = dateObj.getMonth() + 1
+    const day = dateObj.getDay()
+    const year = dateObj.getFullYear()
+
+    return `${month}-${day}-${year}`
+  }
+
+  const search = function (event) {
+
+    event.preventDefault();
+
+    clear();
+
+    const queryURL = buildQueryURL();
+
+    $.ajax({
+      url: queryURL,
+      method: 'GET'
+    }).then(render);
+  }
+
+  const buildQueryURL = function () {
+
+    const baseURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json'
+    const apiKey = 'b9f91d369ff59547cd47b931d8cbc56b:0:74623931'
     
-    const sectionBody = $('#newsFeed');
-    sectionBody.html("");
-    // Section for ajax responses
-//////////////////////////////////////
-////////////////////////////////////
-////////////////////////////////
-////////////////////////////////////
-////////////////////
-    //Section for ajax responses
+    const userInput = $('#search-term').val().trim();
+  
+    let queryURL = `${baseURL}?q=${userInput}&api-key=${apiKey}`;
 
-        }].fail(function (err) {
-            throw err;
-        });
-    };
-    //Eventhandlers//
-    $('#searchButton').on('click', search);
-    $('#clearButton').on('click', clearFields);
+    const startYear = $('#start-year').val().trim();
 
-    $(document).ready(function () {
+    if (parseInt(startYear)) {
+      queryURL += `&begin_date=${startYear}0101`;
+    }
 
-        function clearFields() {
-            $('#searchInput').val("");
-            $('#startYearInput').val("");
-            $('#endYearInput').val("");
-            console.log($('#searchInput').val());
-    
-        }
-    
-        const search = function() {
-            const apikey = "d413b797e2ef42889944fd71f193f81b";
-            const searchInput = $('#searchInput').val();
-            const numberTerm = $('option:selected').val();
-            const startYearInput = $('#startYearInput').val() + `0101`;
-            const endYearInput = $('#endYearInput').val() + `1231`;
-    
-            var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-    
-            url += '?' + $.param({
-                'api-key': apikey,
-                'q': searchinput,
-                'begin_date': startYearInput,
-                'end_date': endYearInput,
-                'page' : 0,
-            });
-    
-            $.ajax({
-                url: url,
-                method: 'GET'
-            }).done(function (response) {
-        
-        const sectionBody = $('#newsFeed');
-        sectionBody.html("");
-        // Section for ajax responses
-    //////////////////////////////////////
-    ////////////////////////////////////
-    ////////////////////////////////
-    ////////////////////////////////////
-    ////////////////////
-        //Section for ajax responses
-    })
-        }].fail(function (err) {
-        throw err;
-    });
-    };
-        //Eventhandlers//
-    $('#searchButton').on('click', search);
-    $('#clearButton').on('click', clearFields);
+    const endYear = $('#end-year').val().trim();
 
+    if (parseInt(endYear)) {
+      queryURL += `&end_date=${endYear}0101`;
+    }
 
+    return queryURL;
+  }
+
+  const clear = function () {
+    $('#article-section').empty();
+  }
+
+  $('#run-search').on('click', search);
+
+  $('#clear-all').on('click', clear);
